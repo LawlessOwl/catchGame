a29422b9a54d259ea6f9145dd50b3b9871647df0
lawlessowl <erokhin-maksim@bk.ru>
2024-11-28 00:41:03 +0300

    feat: added mouse logic

--- state/data.js
+++ state/data.js
@@ -7,14 +7,19 @@ const _state = {
             rowCount: 4,
             columnCount: 4,
         }
+    },
+    positions: {
+        mouse: {x: 0, y: 0},
+        cat1: {x: 2, y:2 },
+        cat2: {x: 3, y:3 }
     }
 }
 
 
-let observer = null
+let _observer = null
 
 export const subsriber = (callback) => {
-    observer = callback
+    _observer = callback
 }
 
 export const getStatus = () => {
@@ -25,9 +30,31 @@ export const getGridSize = () => {
     return _state.settings.gridSize
 }
 
+export const getMousePosition = () => {
+    return _state.positions.mouse
+}
+
 export const gameStart = () => {
     _state.status = GAME_STATUSES.PROGRESS
-    observer()
+    setInterval(_teleportMouse, 1000)
+    _observer()
+}
+
+let _teleportMouse = () => {
+    const newXPosition =  _getRandomInt(getGridSize().columnCount)
+    const newYPosition = _getRandomInt(getGridSize().rowCount)
+
+    if (newXPosition === getMousePosition.x && newYPosition === getMousePosition.y){
+        _teleportMouse();
+        return;
+    }
+    _state.positions.mouse.x = newXPosition
+    _state.positions.mouse.y = newYPosition
+    _observer()
+}
+
+let _getRandomInt = (max) => {
+    return Math.floor(Math.random() * max)
 }
 
 
--- ui/game-settings/grid.component.js
+++ ui/game-settings/grid.component.js
@@ -1,16 +1,21 @@
-import { getGridSize } from "../../state/data.js"
+import { getGridSize, getMousePosition } from "../../state/data.js"
 
 export let Grid = () => {
     const element = document.createElement('table')
 
     const gridSize = getGridSize()
 
+    const mousePosition = getMousePosition()
+
     for (let y = 0; y < gridSize.rowCount; y++) {
         const row = document.createElement("tr")
 
         for (let x = 0; x < gridSize.columnCount; x++) {
             const cell = document.createElement('td')
-            cell.append(x, "-", y)
+            if (x === mousePosition.x && y === mousePosition.y) {
+                cell.append("M")
+            }
+            
             row.append(cell)
             
         }
